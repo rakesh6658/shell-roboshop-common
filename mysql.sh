8ae659ea30f018ab5d90e23cc6256f4fcfab7de8
rakesh <rakeshreddy@gmail.com>
2025-12-26 10:42:00 +0530

    added mysql

---
+++ mysql.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+check_root_user
+dnf install mysql-server -y &>>$log_file
+validate $? "installing mysql"
+
+systemctl enable mysqld &>>$log_file
+validate $? "enabling mysqld"
+
+systemctl start mysqld &>>$log_file
+validate $? "starting mysqld"
+
+mysql_secure_installation --set-root-pass RoboShop@1 &>>$log_file
+validate $? "mysql secure installation setting password"
